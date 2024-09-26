031d1c282b24a3c2db7b7461a1754f799fc5d515
Manju Priya Yadav GS <M.P.Y.Ganesansuresh@tilburguniversity.edu>
2024-09-26 19:10:21 +0200

    added data_preparation_01 rscript in make format

---
+++ src/data_preparation/download_data_01.R
@@ -0,0 +1,19 @@
+# URLs to the IMDb datasets
+urls <- c(
+  "https://datasets.imdbws.com/title.basics.tsv.gz",
+  "https://datasets.imdbws.com/title.episode.tsv.gz",
+  "https://datasets.imdbws.com/title.ratings.tsv.gz"
+)
+
+# Paths to save the datasets in the data folder
+output_files <- c(
+  "data/title_basics.tsv.gz",
+  "data/title_episode.tsv.gz",
+  "data/title_ratings.tsv.gz"
+)
+
+# Download the datasets
+mapply(download.file, urls, output_files)
+
+# Confirmation message
+message("Downloaded IMDb datasets and saved to the 'data' folder.")
