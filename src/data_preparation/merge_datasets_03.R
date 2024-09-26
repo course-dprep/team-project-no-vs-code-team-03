37040f9d89eb24e31d95e668241a9b70471affcd
Manju Priya Yadav GS <M.P.Y.Ganesansuresh@tilburguniversity.edu>
2024-09-26 20:01:13 +0200

    Adding merge_datasets_03 as per make format

---
+++ src/data_preparation/merge_datasets_03.R
@@ -0,0 +1,23 @@
+# merge_datasets.R
+
+# Loading required packages
+library(dplyr)
+library(readr)
+
+# Load cleaned datasets
+title_basics_no_NAs <- read_csv("gen/output/title_basics_no_NAs.csv")
+title_episode_no_NAs <- read_csv("gen/output/title_episode_no_NAs.csv")
+title_ratings <- read_tsv("gen/output/title_ratings.tsv.gz",col_types = cols(.default = "c"))
+
+# # Merging datasets based on tconst (common identifier)
+
+merged_data <- title_basics_no_NAs %>%
+  inner_join(title_ratings, by = "tconst") %>%
+  inner_join(title_episode_no_NAs, by = c("tconst" = "parentTconst"))
+
+
+# Save the merged dataset
+write.csv(merged_data, file = "gen/output/merged_data.csv", row.names = FALSE)
+
+# Confirmation message
+message("Merged datasets and saved the output.")
